f353212c03a3d9394dcc974a4b9b78b4267bbd83
Ajay Raj Merchia <amerchia@moveworks.ai>
2024-03-02 09:22:40 -0800

    Update Pull Request Target

--- ci/check_readme_changes.py
+++ ci/check_readme_changes.py
@@ -2,7 +2,14 @@ import os
 import subprocess
 
 commit_id = os.getenv("GITHUB_SHA")
+print(f"Processing {commit_id}")
 changed_files = subprocess.getoutput(f"git diff --name-only HEAD~1..HEAD")
+
+print("Retrieved the following changed files")
+print(changed_files)
+
+env_file = os.getenv("GITHUB_OUTPUT")
+
 
 comment_message = ""
 for file in changed_files.split("\n"):
@@ -12,4 +19,6 @@ for file in changed_files.split("\n"):
         second_directory = file_parts[1]
         comment_message += f"Changes to {file} can be viewed at https://developer.moveworks.com/creator-studio/resources/{top_directory}?id={second_directory}&commit_id={commit_id}\n"
 
-print(f"::set-output name=comment_message::{comment_message}")
+print(f"Writing output: '{comment_message}' to {env_file}")
+with open(env_file, "a") as f:
+    f.write(f"comment_message={comment_message}\n")
