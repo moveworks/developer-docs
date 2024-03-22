f807c12b7ecdb1443fbeebf4bfb4632e7a5e077e
Indrashis Paul <indrashis.paul@moveworks.ai>
2024-03-22 22:55:06 +0530

    Add Coupa Auth guide (#105)
    * Add Coupa Auth guide
    
    * Add Coupa Auth guide
    
    * Add Coupa Auth guide
    
    * Add Coupa Auth guide
    
    * Add Coupa Auth guide
    
    * Add Coupa Auth guide
    
    * Add Coupa Auth guide

---
+++ authentication-guides/coupa/.codeblocks/block_0.sh
@@ -0,0 +1,6 @@
+curl --location 'https://{{INSTANCE_DOMAIN}}/oauth2/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'grant_type=client_credentials' \
+--data-urlencode 'client_id={{Identifier}}' \
+--data-urlencode 'client_secret={{Secret}}' \
+--data-urlencode 'scope={{SPACE_SEPARATED_LIST_OF_SCOPES}}'
