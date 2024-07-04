ebfbb6e6b1ab9d316fb2b32c2ea8f7dfc5723361
Indrashis Paul <indrashis.paul@moveworks.ai>
2024-07-04 23:41:02 +0530

    Add workday request time-off use case

--- use-case-guides/workday-request-time-off/.codeblocks/block_1.sh
+++ use-case-guides/workday-request-time-off/.codeblocks/block_1.sh
@@ -3,6 +3,6 @@ curl --location 'https://{{domain}}.workday.com/ccx/api/wql/v1/{{instance}}/data
 --header 'Authorization: Bearer {{access_token}}' \
 --data-raw '
     {
-        "query": "SELECT workdayAccount, workdayID, email_PrimaryWorkOrPrimaryHome, position, cf_TimeOffBalanceForPTOAndVacationPlans, eligibleAndVisibleTimeOffPlansForWorkerAsOfDate, allEligibleTimeOffsForWorker FROM indexedAllWorkers (dataSourceFilter = indexedAllWorkersFilter, includeSubordinateOrganizations = false, isActive = false) WHERE email_PrimaryWorkOrPrimaryHome = 'test@workday.net'"
+        "query": "SELECT workdayAccount, workdayID, email_PrimaryWorkOrPrimaryHome, position, cf_TimeOffBalanceForPTOAndVacationPlans, eligibleAndVisibleTimeOffPlansForWorkerAsOfDate, allEligibleTimeOffsForWorker FROM indexedAllWorkers (dataSourceFilter = indexedAllWorkersFilter, includeSubordinateOrganizations = false, isActive = false) WHERE email_PrimaryWorkOrPrimaryHome = '{{user.email_addr}}'"
     }
 '
