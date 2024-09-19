7763e047ae80d9081d8716b8e0ab0b6bf806edf5
Ajay Raj Merchia <amerchia@moveworks.ai>
2024-09-19 14:07:09 -0400

    Remove old library

--- authentication-guides/microsoft-graph-api/.codeblocks/block_0.sh
+++
@@ -1,6 +0,0 @@
-curl --location 'https://login.microsoftonline.com/{{tenant_id}}/oauth2/token' \
---header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'client_id={{application_id}}' \
---data-urlencode 'client_secret={{client_secret}}' \
---data-urlencode 'resource=https://graph.microsoft.com' \
---data-urlencode 'grant_type=client_credentials'
--- authentication-guides/salesforce/.codeblocks/block_0.sh
+++
@@ -1,9 +0,0 @@
-curl --location 'https://login.salesforce.com/services/oauth2/token' \
- --header 'Accept: application/json' \
- --header 'Content-Type: application/x-www-form-urlencoded' \
- --data-urlencode 'grant_type=password' \
- --data-urlencode 'client_id={{salesforce_consumer_key}}’ \
- --data-urlencode 'client_secret={{salesforce_consumer_secret}}' \
- --data-urlencode 'username={{salesforce_username}}' \
- --data-urlencode 'password={{salesforce_password}}{{salesforce_security_token}}' \
- --data-urlencode 'redirect_uri=https://login.salesforce.com/'
