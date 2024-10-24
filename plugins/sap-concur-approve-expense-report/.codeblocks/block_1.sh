7f0eaeb4c926a68865ec7ec4a2e11ea4f9a3b007
Indrashis Paul <indrashis.paul@moveworks.ai>
2024-10-24 11:07:44 +0530

    Add Concur Expense Approvals guide

---
+++ plugins/sap-concur-approve-expense-report/.codeblocks/block_1.sh
@@ -0,0 +1,3 @@
+curl --location --request GET 'https://us2.api.concursolutions.com/profile/identity/v4/users?count=100&attributes=displayName%2Cid%2CuserName' \
+--header 'Accept: application/json' \
+--header 'Authorization: Bearer {auth_token}' \
