8044d51614df0d9e2ed8f43597621ee55abb4938
Shreya Bhadwal <sbhadwal@moveworks.ai>
2025-01-08 17:31:39 +0530

    Fix validation error

--- plugins/servicenow-user-assets/.codeblocks/block_0.sh
+++ plugins/servicenow-user-assets/.codeblocks/block_0.sh
@@ -1,3 +1,3 @@
 curl --request GET \
---url '[https://your-domain.service-now.com/api/now/table/alm_asset?](https://api/now/table/alm_asset?sysparm_query=assigned_to.email%3Dexample%40email.com&sysparm_fields=model_category.name,display_name,asset_tag)sysparm_query=assigned_to.email%3Dexample%[40email.com](http://40email.com/)&sysparm_fields=model_category.name%2C%20display_name%2C%20asset_tag\
+--url 'https://your-domain.service-now.com/api/now/table/alm_asset?sysparm_query=assigned_to.email%3Dexample%40email.com&sysparm_fields=model_category.name%2C%20display_name%2C%20asset_tag\'
 --header 'Authorization: Bearer YOUR_ACCESS_TOKEN'
