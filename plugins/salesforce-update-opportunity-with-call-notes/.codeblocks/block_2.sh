330d82e1a6c90759a5a4c666180f2a02061bc90b
hariv24 <hariv.k24@gmail.com>
2025-01-22 15:00:48 +0530

    python

---
+++ plugins/salesforce-update-opportunity-with-call-notes/.codeblocks/block_2.sh
@@ -0,0 +1,9 @@
+curl --location 'https://<YOUR_DOMAIN>.my.Salesforce.com/services/data/v63.0/sobjects/Task' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>' \
+--header 'Content-Type: application/json' \
+--data '{
+    "Subject": "Call Summary",
+    "Description": "<CALL_SUMMARY>",
+    "WhatId": "<OPPORTUNITY_ID>",
+    "Status": "Completed"
+}'
