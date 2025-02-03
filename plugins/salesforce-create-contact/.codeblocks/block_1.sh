4613710bffa25555f2c8216c662e3e066d1c08e0
Indrashis Paul <indrashis.paul@moveworks.ai>
2025-02-03 14:56:22 +0530

    Create contact in Salesforce guide (#267)

---
+++ plugins/salesforce-create-contact/.codeblocks/block_1.sh
@@ -0,0 +1,10 @@
+curl --location 'https://<YOUR_DOMAIN>.my.salesforce.com/services/data/v62.0/sobjects/Contact' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: <YOUR_AUTH_TOKEN>' \
+--data '{
+"FirstName": "<FIRST_NAME>",
+"LastName": "<LAST_NAME>",
+"Email": "<EMAIL>",
+"Phone": "<Phone>",
+"AccountId": "<ACCOUNT_ID>"
+}'
