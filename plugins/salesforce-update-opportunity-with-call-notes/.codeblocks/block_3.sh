f4d98acdb511530f59bff949e91af65527df3e50
Hariv Kannan <110089212+hariv24@users.noreply.github.com>
2025-02-03 16:13:37 +0530

    Salesforce-Update-Opportunity-With-CallNotes (#271)
    * Add Salesforce integration scripts for updating opportunities and logging call summaries
    
    * image Q
    
    ---------
    
    Co-authored-by: Indrashis Paul <indrashis.paul@moveworks.ai>

---
+++ plugins/salesforce-update-opportunity-with-call-notes/.codeblocks/block_3.sh
@@ -0,0 +1,2 @@
+curl --location 'https://<YOUR_DOMAIN>.my.Salesforce.com/services/data/v63.0/query/?q=SELECT%20Id%2C%20Name%20FROM%20Opportunity%20WHERE%20Account.Name%20%3D%20%27<ACCOUNT_NAME>%27%20AND%20StageName%20%3D%20%27Qualify%27' \
+--header 'Content-Type: application/json' \
