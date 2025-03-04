5e50339f071e46acc5b6cce0eb88a8f5619c782c
Monivijayababu <monika.a@arbaan.com>
2025-03-04 23:21:20 +0530

    Lookup_Account_Renewal

---
+++ plugins/salesforce-account-renewal-lookup/.codeblocks/block_2.sh
@@ -0,0 +1,3 @@
+curl --request GET \
+--location 'https://<YOUR_DOMAIN>/services/data/v62.0/query?q=SELECT%20Id%2CAccount.Name%2CAccount.Id%2CStartDate%2C%20EndDate%2C%20Status%2CContractTerm%20FROM%20Contract%20WHERE%20Account.Name%20LIKE%20%27{Account_Name}%25%27' \
+--header 'Content-Type: application/json' \
