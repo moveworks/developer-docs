3bedd8fc6fd908191f29b5765bb675f02f0e8617
vijval-n <vijval.narayana@moveworks.ai>
2025-03-25 11:56:51 +0530

    Split into two Plugins

--- plugins/asana-list-projects/.codeblocks/block_3.sh
+++
@@ -1,4 +0,0 @@
-curl --request GET \
-     --url 'https://app.asana.com/api/1.0/projects?workspace=<workspace_gid>&team={{team_gid}}&opt_fields=completed' \
-     --header 'accept: application/json' \
-     --header 'authorization: Bearer <Your Asana PAT>'
