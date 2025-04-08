e89e21b6e1701985b122ea2f46bfb704b59af81b
Indrashis Paul <indrashis.paul@moveworks.ai>
2025-04-08 19:02:48 +0530

    Updated the folder traversal to reflect recent changes from Notion wh… (#376)
    Updated the folder traversal to reflect recent changes from Notion while exporting.

--- ci/copy_from_notion.sh
+++ ci/copy_from_notion.sh
@@ -20,6 +20,14 @@ unzip -o "$latest_zip" -d unzipped_content
 # Move into the unzipped directory (assuming it has the same name as the .zip without the extension)
 cd unzipped_content
 
+# Check if the "Private and Shared" folder exists
+if [ -d "Private & Shared" ]; then
+    cd "Private & Shared"
+else
+    echo "The 'Private & Shared' folder was not found in the unzipped content."
+    exit 1
+fi
+
 # Find the specific file and move it to the current user's directory from which the script was run
 specific_file="Plugin Research df47317a8eb449178020d6bf3dec4b23_all.csv"
 
@@ -32,7 +40,7 @@ else
     echo "File not found in the unzipped content."
 fi
 
-cd ..
+cd ../..
 rm -r unzipped_content
 rm $latest_zip
 
