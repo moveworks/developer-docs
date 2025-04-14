31a7476121089c8cbc98ee3878973681324a523f
Deepan Shukla <deepan.shukla@moveworks.com>
2025-04-14 19:31:21 +0530

    added sto_add_comment Guide

---
+++ plugins/stackoverflow-add-comment/.codeblocks/block_0.sh
@@ -0,0 +1,2 @@
+curl --request POST "https://{your_organisation}.stackenterprise.co/api/2.3/posts/{{id}}/comments/add?body={{comment}}&key={{your_key}}" \
+     --header "Authorization: Bearer {{access_token}}"
