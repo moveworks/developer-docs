b8207132f3f3f42bdc51afcc12fd962f1070c05d
Sudheer <ssudheer@moveworks.ai>
2025-04-14 21:51:40 +0530

    Lookup a User’s Spots (#438)
    Co-authored-by: Indrashis Paul <indrashis.paul@moveworks.ai>

---
+++ plugins/highspot-lookup-spots/.codeblocks/block_2.sh
@@ -0,0 +1,4 @@
+curl --location 'https://api.highspot.com/v1.0/spots' \
+--header 'Content-Type: application/json' \
+--header 'hs-user: your.name@company.com' \
+--header 'Authorization: Basic dX**********************'
