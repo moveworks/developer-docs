5dab05251984f8f6f731d32f3b482d16cab5174d
Deepan Shukla <deepan.shukla@moveworks.com>
2025-04-14 15:05:05 +0530

    cdoe block change

--- connectors/stackoverflow/.codeblocks/block_0.sh
+++ connectors/stackoverflow/.codeblocks/block_0.sh
@@ -1,1 +1,1 @@
-https://{{your organization}}.stackenterprise.co/oauth?client_id=4&scope=write_access;no_expiry&redirect_uri=https://localhost.com
+https://{{your organization}}.stackenterprise.co/oauth?client_id=4&scope=write_access,no_expiry&redirect_uri=https://localhost.com
