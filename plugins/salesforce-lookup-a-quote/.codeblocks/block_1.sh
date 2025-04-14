1232105f2250b93d2e4b9cbc67d88a7737322981
Indrashis Paul <indrashis.paul@moveworks.ai>
2025-04-14 19:04:32 +0530

    Resync to Notion resync_2025_04_14 (#440)

--- plugins/salesforce-lookup-a-quote/.codeblocks/block_1.sh
+++
@@ -1,3 +0,0 @@
-curl --location 'https://<YOUR_DOMAIN>/services/data/v62.0/query/?q=SELECT+Id,+Name,+CreatedDate,+Status,+ExpirationDate,+TotalPrice,+AccountId,+Discount+FROM+Quote+WHERE+QuoteNumber+LIKE+%27%25<QUOTE_NUMBER>%25%27' \
---header 'Authorization: Bearer <ACCESS_TOKEN>'
-
