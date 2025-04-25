247f064bf8514dbd85227a38a89d38b1c67bfa71
Indrashis Paul <indrashis.paul@moveworks.ai>
2025-04-26 01:06:42 +0530

    Resync to Notion resync_2025_04_26

--- plugins/servicenow-summarise-ticket-history/.codeblocks/block_0.sh
+++
@@ -1,2 +0,0 @@
-curl --location 'https://<instance>.service-now.com/api/now/table/sys_history_line?sysparm_fields=new%2Cold%2Clabel%2Cupdate&sysparm_query=set.id%3D<TICKET_ID>%5ElabelINResolution%20notes%2CResolution%20Code%2CPriority%2CUrgency%2CNumber%2CShort%20description%2CAdditional%20comments%2CAssignment%20group%2CAssigned%20to%2CState' \
---header 'Authorization: Bearer <ACCESS_TOKEN>'
