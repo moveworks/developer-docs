d874f4c9cb2ef0bafec3004b3ccb401261e0945f
Jagadeesh-ragav <159127109+Jagadeesh-ragav@users.noreply.github.com>
2025-05-30 19:55:58 +0530

    Workday View Payslips (#488)
    * worday find start date
    
    * workday view payslips
    
    * Update API documentation to clarify payslip retrieval endpoint
    
    * Update README to include Workday permissions
    
    * Add pagination parameters to payslip retrieval API call
    
    * Update API endpoint and documentation for retrieving worker details by email
    
    * Update API endpoint in documentation to use generic tenant placeholder
    
    * Update block_0.sh
    
    ---------
    
    Co-authored-by: Indrashis Paul <indrashis.paul@moveworks.ai>

---
+++ plugins/workday-view-payslips/.codeblocks/block_1.sh
@@ -0,0 +1,3 @@
+curl --location 'https://<API_SERVER_DOMAIN>/ccx/api/common/v1/<TENANT>/workers/{WORKER_ID}/paySlips?limit=48&offset=0' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>' \
+--header 'Content-Type: application/json'
