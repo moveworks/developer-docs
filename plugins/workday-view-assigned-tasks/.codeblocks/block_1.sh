5801ebf320208a2295d31c2dd0e82aa3611ff9f9
IswaryaArbaan <iswarya.a@arbaan.com>
2025-05-30 15:47:59 +0530

    workday_view_assigned_task

--- plugins/workday-view-assigned-tasks/.codeblocks/block_1.sh
+++ plugins/workday-view-assigned-tasks/.codeblocks/block_1.sh
@@ -1,3 +1,3 @@
-curl --location 'https://<API_SERVER_DOMAIN>/ccx/api/v1/<TENANT>/workers/{{WORKER_ID}}/inboxTasks' \
+curl --location 'https://<API_SERVER_DOMAIN>/ccx/api/v1/<TENANT>/workers/{{worker_id}}/inboxTasks' \
 --header 'Authorization: Bearer <ACCESS_TOKEN>' \
 --header 'Content-Type: application/json'
