7191028ee1baea2406d2cb6244e69a0ab4afbfc3
hariv24 <hariv.k24@gmail.com>
2025-06-02 16:45:24 +0530

    made the requested changes

--- plugins/sap-success-factors-view-payslips/.codeblocks/block_0.sh
+++
@@ -1,4 +0,0 @@
-curl --location --globoff 'https://<YOUR_INSTANCE>/odata/v2/User?%24filter=email%20eq%20%27{{email}}%27&%24select=userId%2CfirstName%2ClastName%2CdisplayName%2Cnickname%2Cemail%2Clocation%2Cdepartment%2Cstatus%2Cgender%2CtimeZone%2CemployeeClass%2ChireDate%2CimpactOfLoss%2CriskOfLoss%2CjobCode%2CbusinessPhone%2Csalary%2CcompensationSalaryRateType%2CempId' \
---header 'Accept: application/json' \
---header 'Authorization:  \
---data ''
--- plugins/sap-success-factors-view-payslips/.codeblocks/block_1.sh
+++
@@ -1,4 +0,0 @@
-curl --location 'https://<YOUR_INSTANCE>/odata/v2/PicklistOption?%24filter=picklist%20eq%20%27PersonPronouns%27&%24select=id%2CexternalCode' \
---header 'Authorization: Bearer \
---header 'Accept: application/json' \
-
