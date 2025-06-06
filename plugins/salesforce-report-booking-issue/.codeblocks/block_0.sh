a73cfcdf664a18266fe1b1405355a4429213c006
tamilselvan.s <tamilselvan.s@arbaan.com>
2025-06-06 11:49:14 +0530

    issues resolved

--- plugins/salesforce-report-booking-issue/.codeblocks/block_0.sh
+++ plugins/salesforce-report-booking-issue/.codeblocks/block_0.sh
@@ -1,2 +1,2 @@
-curl --location 'https://<API_Server_domain>/services/data/v63.0/query/?q=SELECT+Id%2C+Name%2C+Status%2C+Owner.Email+FROM+Quote+WHERE+Owner.Email%3D<email>+AND+Name%3D<quote_name>' \
+curl --location 'https://<API_Server_domain>/services/data/v63.0/query?q=SELECT+Id%2C+Name%2C+Status%2C+Status_Comment__c%2C+Owner.Email+FROM+Quote+WHERE+Owner.Email%3D%27<email>%27+AND+Name%3D%27<quote_name>%27+AND+Status+NOT+IN+(%27Approved%27%2C%27Accepted%27)' \
 --header 'Authorization: Bearer <Your Access Token>'
