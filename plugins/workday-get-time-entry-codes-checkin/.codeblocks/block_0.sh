c7ea2f3445152632f2c43e292b5020e5d91e97a4
hariv24 <hariv.k24@gmail.com>
2025-06-11 06:58:11 +0530

    Check-In Time Entry Code

--- plugins/workday-get-time-entry-codes-checkin/.codeblocks/block_0.sh
+++ plugins/workday-get-time-entry-codes-checkin/.codeblocks/block_0.sh
@@ -1,4 +1,9 @@
-curl --location 'https://<YOUR_DOMAIN>/ccx/api/wql/v1/<INSTANCE_ID>/query/?q=SELECT workdayID, fullName, email_PrimaryWorkOrPrimaryHome, timeZoneOfLocationOfWorkersPrimaryPosition, check_InStatus FROM allWorkers WHERE fullName = {{fullName}}' \
+curl --request POST \
+curl --location 'https://<DOMAIN>.myworkday.com/api/wql/v1/<TENANT>/data?offset=0&limit=1' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>' \
 --header 'Content-Type: application/json' \
---header 'Authorization: Bearer <ACCESS_TOKEN>'
+--data '{
+    "query": "SELECT workdayID, fullName,businessTitle, email_PrimaryWorkOrPrimaryHome,employeeID FROM allWorkers WHERE email_PrimaryWorkOrPrimaryHome = '{{email_PrimaryWorkOrPrimaryHome}}'"
+}
+'
 
--- plugins/workday-get-time-entry-codes-checkin/.codeblocks/block_1.sh
+++ plugins/workday-get-time-entry-codes-checkin/.codeblocks/block_1.sh
@@ -1,4 +1,3 @@
 curl --location 'https://<YOUR_DOMAIN>/api/timeTracking/v3/<INSTANCE_ID>/values/timeTypes/timeEntryCodes?date=<DATE>&inOutCodeOnly=true&worker=<WORKER_ID>' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Bearer <ACCESS_TOKEN>'
-
