d82670844b97fce1cd57afb65946d76c61acc068
jagadeesh.r <jagadeesh.r@arbaan.com>
2025-06-20 14:20:14 +0530

    Add API calls for retrieving user and computer refresh dates

---
+++ plugins/servicenow-computer-refresh-date/.codeblocks/block_0.sh
@@ -0,0 +1,4 @@
+curl --location 'https://<YOUR_INSTANCE>/api/now/table/sys_user?sysparm_query=email=<USER_EMAIL>' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>' \
+--header 'Accept: application/json'
+
