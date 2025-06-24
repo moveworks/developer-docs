8249c47b6b066c6daaf027cfc05c32dea0498463
manish <manishkumar.v@arbaan.com>
2025-06-24 17:47:39 +0530

    Look up My Cost Center

--- plugins/servicenow-lookup-cost-center/.codeblocks/block_1.sh
+++ plugins/servicenow-lookup-cost-center/.codeblocks/block_1.sh
@@ -1,1 +1,3 @@
-curl --location --request GET 'https://your-instance.service-now.com/api/now/table/cmn_cost_center'
+curl --location "https://<YOUR_INSTANCE>/api/now/table/cmn_cost_center?sysparm_query=id=<COST_CENTER_ID>&sysparm_display_value=true" \
+--header "Authorization: Bearer <ACCESS_TOKEN>" \
+--header "Accept: application/json"
