ef88591cfbf815be581582b444dd2bd6f67a4e89
sandhiya376 <sandhiya.s@arbaan.com>
2025-06-25 11:15:00 +0530

    servicenow_check_employee_id

---
+++ plugins/servicenow-check-employee-id/.codeblocks/block_0.sh
@@ -0,0 +1,3 @@
+curl --location 'https://<YOUR_INSTANCE>.service-now.com/api/now/table/sys_user?sysparm_query=email%3D<USER_EMAIL>&sysparm_fields=name%2Cemail%2Cemployee_number' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>' \
+--header 'Accept: application/json' 
