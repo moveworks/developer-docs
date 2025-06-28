a9d52c1ca425c0e0b9bbab62f1bf8ef7044874ee
Indrashis Paul <indrashis.paul@moveworks.ai>
2025-06-28 10:33:44 +0530

    Resync to Notion resync_2025_06_28 (#621)

--- plugins/servicenow-computer-refresh-date/.codeblocks/block_0.sh
+++
@@ -1,4 +0,0 @@
-curl --location 'https://<YOUR_INSTANCE>/api/now/table/sys_user?sysparm_query=email=<USER_EMAIL>' \
---header 'Authorization: Bearer <ACCESS_TOKEN>' \
---header 'Accept: application/json'
-
