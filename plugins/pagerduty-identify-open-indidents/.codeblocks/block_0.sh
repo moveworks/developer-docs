9496de6396a58967d59ab3653309037983f8ee33
Indrashis Paul <indrashis.paul@moveworks.ai>
2025-07-01 11:24:42 +0530

    Resync to Notion resync_2025_07_01

--- plugins/pagerduty-identify-open-indidents/.codeblocks/block_0.sh
+++
@@ -1,2 +0,0 @@
-curl --location --globoff 'https://api.pagerduty.com/incidents?since={{from_date}}&until={{date_until}}&statuses[]=triggered&statuses[]=acknowledged&limit=100' \
---header 'Authorization: Token <Your Access Token>'
