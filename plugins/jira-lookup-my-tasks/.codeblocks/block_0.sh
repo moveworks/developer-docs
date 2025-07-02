e877bbe797cada0e0cace1586b811c556b9b3870
jagadeesh.r <jagadeesh.r@arbaan.com>
2025-07-02 10:14:21 +0530

    Update Jira API endpoint from v2 to v3 in curl commands

--- plugins/jira-lookup-my-tasks/.codeblocks/block_0.sh
+++ plugins/jira-lookup-my-tasks/.codeblocks/block_0.sh
@@ -1,4 +1,4 @@
 curl --request GET \
-  --url 'https://<YOUR_INSTANCE>/rest/api/2/search?fields=summary,status,project,assignee&jql=assignee = "<EMAIL_ID>" AND statusCategory != "Done"&maxResults=100' \
+  --url 'https://<YOUR_INSTANCE>/rest/api/3/search?fields=summary,status,project,assignee&jql=assignee = "<EMAIL_ID>" AND statusCategory != "Done"&maxResults=100' \
   --user 'email@example.com:<api_token>' \
   --header 'Accept: application/json'
