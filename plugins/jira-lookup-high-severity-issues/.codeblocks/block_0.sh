9da9dbe6c821415dfe5d99e000ccb2b7b4a9bd46
Monivijayababu <monika.a@arbaan.com>
2025-07-09 15:48:33 +0530

    Updated document

--- plugins/jira-lookup-high-severity-issues/.codeblocks/block_0.sh
+++ plugins/jira-lookup-high-severity-issues/.codeblocks/block_0.sh
@@ -1,4 +1,4 @@
 curl --request GET
---location 'https://<YOUR_DOMAIN>/rest/api/3/search?jql=statusCategory%20!%3D%20Done%20AND%20priority%20IN%20(%22Highest%22%2C%20%22High%22)%20AND%20created%20%3E%3D%20-14d&fields=key%2Csummary%2Cstatus%2Cpriority%2Ccreated' \
+--location 'https://<YOUR_DOMAIN>/rest/api/3/search?jql=statusCategory%20!%3D%20Done%20AND%20priority%20IN%20(%22Highest%22%2C%20%22High%22)%20AND%20created%20%3E%3D%20-{{days}}d&fields=key%2Csummary%2Cstatus%2Cpriority%2Ccreated' \
 --header 'Authorization: Bearer <ACCESS_TOKEN>' \
 --header 'Accept: application/json' \
