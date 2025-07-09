439711dffcde563876cf9ec2aa97e52998c27c3f
IswaryaArbaan <iswarya.a@arbaan.com>
2025-07-09 12:20:17 +0530

    Add issue type dynamic

--- plugins/jira-update-bulk-issue-priority/.codeblocks/block_0.sh
+++ plugins/jira-update-bulk-issue-priority/.codeblocks/block_0.sh
@@ -1,3 +1,3 @@
-curl --location 'https://<YOUR_INSTANCE>/rest/api/3/search?jql=issuetype=Bug AND assignee="{{email}}" AND statusCategory!=Done AND created >= {{created_range}}' \
+curl --location 'https://<YOUR_INSTANCE>/rest/api/3/search?jql=issuetype={{issue_type}} AND assignee="{{email}}" AND statusCategory!=Done AND created >= {{created_range}}' \
 --header 'Accept: application/json' \
 --header 'Authorization: Basic <API_TOKEN>'
