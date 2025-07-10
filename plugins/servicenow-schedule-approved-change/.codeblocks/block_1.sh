654accfb5861a7ceb4ebc01c769989c5ff525045
Haritha v <haritha.v@arbaan.com>
2025-07-10 12:49:37 +0530

    Servicenow_Schedule_a_change_updated

--- plugins/servicenow-schedule-approved-change/.codeblocks/block_1.sh
+++ plugins/servicenow-schedule-approved-change/.codeblocks/block_1.sh
@@ -1,8 +1,4 @@
-curl --location --request PATCH 'https://<YOUR_DOMAIN>/api/now/table/change_request/{{sys_id}}' \
+curl --location 'https://<YOUR_DOMAIN>/api/now/table/change_request?sysparm_query=short_descriptionLIKE{{short_desc}}^approval=approved&sysparm_fields=sys_id,number,approval,state,short_description' \
 --header 'Authorization: Bearer <ACCESS_TOKEN>' \
 --header 'Accept: application/json' \
---header 'Content-Type: application/json' \
---data '{
-  "start_date": "{{start_date}}",
-  "end_date": "{{end_date}}"
-}'
+--header 'Content-Type: application/json'
