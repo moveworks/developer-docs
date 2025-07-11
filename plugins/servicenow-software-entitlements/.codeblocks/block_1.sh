1ec4db305ff06fcfa9ba69dd11a4f4cb2321d5ac
manish <manishkumar.v@arbaan.com>
2025-07-11 12:49:57 +0530

    Look Up My Software Entitlements

---
+++ plugins/servicenow-software-entitlements/.codeblocks/block_1.sh
@@ -0,0 +1,4 @@
+curl --location 'https://<YOUR_INSTANCE>/api/now/table/alm_entitlement?sysparm_query=assigned_to=<USER_SYS_ID>' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>' \
+--header 'Accept: application/json' \
+--header 'Content-Type: application/json'
