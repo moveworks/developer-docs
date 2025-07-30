7054f937fec0d37a8bb34ac0672dd7f7a8e11f0a
sandhiya376 <sandhiya.s@arbaan.com>
2025-07-30 10:22:56 +0530

    servicenow_Create_Change_Request_from_an_Incident_new

---
+++ plugins/servicenow-change-request-from-incident/.codeblocks/block_2.sh
@@ -0,0 +1,3 @@
+curl --location 'https://<YOUR_INSTANCE>.service-now.com/api/now/table/chg_model?sysparm_query=nameLIKE<MODEL_NAME>' \
+--header 'Accept: application/json' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>'
