3150ed4bdd2c4ce862cb9603ec6bcc35c2a88741
Praveenramesh12 <rpraveen1dme@gmail.com>
2025-07-31 11:57:10 +0530

    Look up Upcoming Visa Expirations

---
+++ plugins/workday-look-up-upcoming-visa-expirations/.codeblocks/block_0.sh
@@ -0,0 +1,6 @@
+curl --location 'https://<your-instance>/ccx/api/wql/v1/<TENANT>/data' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer <your-access-token>' \
+--data-raw '{
+"query": "SELECT worker, jobFamilyGroup, email_PrimaryWorkOrPrimaryHome, workdayID FROM allActiveEmployees WHERE jobFamilyGroup = \"<jobFamilyGroup-ID>\" AND email_PrimaryWorkOrPrimaryHome = \"<email>\""
+}'
