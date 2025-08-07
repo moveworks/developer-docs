92e5bcc407ab07caf68092dbd52c370afebf8670
Monivijayababu <monika.a@arbaan.com>
2025-08-07 14:14:43 +0530

    View_Benefit_Elections - Updated CURL

--- plugins/workday-view-benefits-elections/.codeblocks/block_0.sh
+++ plugins/workday-view-benefits-elections/.codeblocks/block_0.sh
@@ -1,5 +1,5 @@
 curl --request POST \
-curl --location 'https://<DOMAIN>.myworkday.com/api/wql/v1/<TENANT>/data?offset=0&limit=1' \
+curl --location 'https://<DOMAIN>.myworkday.com/api/wql/v1/<TENANT>/data' \
 --header 'Content-Type: application/json' \
 --data '{
     "query": "SELECT workdayID, fullName, email_PrimaryWork, benefitElections{benefitPlan, coverage, dependents, beneficiaries_All, coverageBeginDateForBenefitPlan, deductionBeginDateForBenefitPlan, employerContributionPerBenefitPlanFrequency, employeeCostPerBenefitPlanFrequency,currentlyEnrolledAsOfDate}, benefitPlans_CurrentlyEnrolled FROM indexedAllWorkers (dataSourceFilter = indexedAllWorkersFilter, includeSubordinateOrganizations = true, isActive = false) WHERE email_PrimaryWork = '\''{{email}}'\''"
