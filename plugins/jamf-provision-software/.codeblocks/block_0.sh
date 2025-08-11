174a1d594223f4309edd6d97f7fdd69f861aed7f
balakrishnan <balakrishnan.k@arbaan.com>
2025-08-11 19:08:13 +0530

    jamf provision

---
+++ plugins/jamf-provision-software/.codeblocks/block_0.sh
@@ -0,0 +1,4 @@
+curl --location 'https://<YOUR_INSTANCE>/api/v1/packages?filter=packageName%3D%3D%22<package_name>%22' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer <ACCESS_TOKEN>'
+
