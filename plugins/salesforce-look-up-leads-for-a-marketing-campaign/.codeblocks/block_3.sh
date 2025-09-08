c28656ad1171fe3f140b0edbf2d1d7cd0f250117
Jishnu Sripada <sjishnu+movester@moveworks.ai>
2025-09-08 15:37:32 +0530

    Salesforce Lookup Leads for a Marketing Campaign V3
    Salesforce Lookup Leads for a Marketing Campaign V3

---
+++ plugins/salesforce-look-up-leads-for-a-marketing-campaign/.codeblocks/block_3.sh
@@ -0,0 +1,1 @@
+SELECT Type, LeadId, Lead.FirstName, Lead.LastName, Lead.Email, Lead.Company, Lead.Status FROM CampaignMember WHERE CampaignId = '601Ed00000KQay1IAD' AND LeadId != NULL AND Type='Lead'
