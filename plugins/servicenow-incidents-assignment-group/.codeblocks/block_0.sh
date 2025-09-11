324b89c916c0824dd5f9cd276bbfb4aab7301844
sbhadwal-moveworks <sbhadwal@moveworks.ai>
2025-09-11 22:47:38 +0530

    Resync to Notion resync_2025_09_11

--- plugins/servicenow-incidents-assignment-group/.codeblocks/block_0.sh
+++
@@ -1,3 +0,0 @@
-curl --location 'https://<YOUR_INSTANCE>/api/now/table/sys_user?sysparm_query=user.email=<USER_EMAIL>&sysparm_fields=group,sys_id,user.email&sysparm_display_value=all' \
---header 'Authorization: Bearer <ACCESS_TOKEN>' \
---header 'Accept: application/json'
