57bcf23082730ee8599c2aec7dcf78fb7af5246e
sbhadwal-moveworks <sbhadwal@moveworks.ai>
2025-09-11 22:37:16 +0530

    Resync to Notion resync_2025_09_11

--- plugins/salesforce-look-up-leads-for-a-marketing-campaign/.codeblocks/block_1.sh
+++
@@ -1,1 +0,0 @@
-SELECT Id, CreatedDate, Name FROM Campaign WHERE Name LIKE '%Q4%Campaign%2025%' ORDER BY CreatedDate DESC
--- plugins/salesforce-look-up-leads-for-a-marketing-campaign/.codeblocks/block_3.sh
+++
@@ -1,1 +0,0 @@
-SELECT Type, LeadId, Lead.FirstName, Lead.LastName, Lead.Email, Lead.Company, Lead.Status FROM CampaignMember WHERE CampaignId = '601Ed00000KQay1IAD' AND LeadId != NULL AND Type='Lead'
