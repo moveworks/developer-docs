ccc1c54aa29bffc6cbdab83e292b0bc8e48d1a17
manish <manishkumar.v@arbaan.com>
2025-09-12 18:26:15 +0530

    Salesforce Delete A Contact

---
+++ plugins/salesforce-delete-contact/.codeblocks/block_2.sh
@@ -0,0 +1,3 @@
+curl --location 'https://<YOUR_INSTANCE>/services/data/v56.0/query/?q=SELECT+Id%2CName%2CAccount.Name%2CTitle%2CEmail%2CCreatedDate%2CLastActivityDate+FROM+Contact+WHERE+Account.Name+LIKE+%27%25{{account_name}}%25%27+ORDER+BY+LastModifiedDate+DESC+LIMIT+100' \
+--header 'Authorization: Bearer {{access_token}}' \
+--header 'Content-Type: application/json'
