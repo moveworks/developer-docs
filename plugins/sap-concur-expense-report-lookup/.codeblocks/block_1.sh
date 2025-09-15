fb7eb06eaae31e900872dfcb8ce65bdf76024b6b
sudheer <ssudheer@moveworks.ai>
2025-09-15 17:51:05 +0530

    SAP Concur Look Up Expense Reports

---
+++ plugins/sap-concur-expense-report-lookup/.codeblocks/block_1.sh
@@ -0,0 +1,4 @@
+curl --location 'https://your.domain.com/api/v3.0/expense/entries?user={{user_email_address}}&report_id=<report_id>' \
+--header 'Accept: application/json' \
+--header 'Authorization: Bearer {{generated_bearer_token}}'
+
