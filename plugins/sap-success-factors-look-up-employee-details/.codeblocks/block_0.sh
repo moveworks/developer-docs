203ca653d258770e2915236d527b479d0f692309
Jishnu Sripada <sjishnu+movester@moveworks.ai>
2025-09-19 12:49:10 +0530

    SAP Success Factors Look Up Employee Information V2
    Updated guide

--- plugins/sap-success-factors-look-up-employee-details/.codeblocks/block_0.sh
+++ plugins/sap-success-factors-look-up-employee-details/.codeblocks/block_0.sh
@@ -1,5 +1,5 @@
 curl --request GET \
-  --location 'https://<API_SERVER>/odata/v2/User?$filter=tolower(displayName)%20like%20%27{{employee_name}}%25%27&$select=userId,username,displayName,email,gender,dateOfBirth,hireDate,country,location,division,department,title,jobLevel,payGrade,status,manager/userId,manager/displayName,manager/email&$expand=manager' \
+  --location 'https://<API_SERVER>/odata/v2/User?$filter=displayName%20like%20%27displayName%25%27&$select=userId,username,displayName,email,gender,dateOfBirth,hireDate,country,location,division,department,title,jobLevel,payGrade,status,manager/userId,manager/displayName,manager/email&$expand=manager' \
   --header 'Authorization: Bearer <YOUR_ACCESS_TOKEN>' \
   --header 'Content-Type: application/json' \
   --header 'Accept: application/json'
