41c14512320acb35c551b664cf302a71153dcbed
sjishnu-moveworks <sjishnu@moveworks.com>
2025-10-21 15:55:34 +0530

    V1 Update block_0.sh

--- plugins/sailpoint-inow-look-up-user-account-details/.codeblocks/block_0.sh
+++ plugins/sailpoint-inow-look-up-user-account-details/.codeblocks/block_0.sh
@@ -1,10 +1,13 @@
-{
+curl --location 'https://<YOUR_INSTANCE>.api.identitynow.com/v3/search' \
+--header 'Authorization: Bearer <YOUR_ACCESS_TOKEN>' \
+--header 'Accept: application/json' \
+--header 'Content-Type: application/json' \
+--data '{
+  "indices": ["identities"],
   "query": {
     "query": "{{filter_query}}"
   },
- 
-  "indices": ["identities"],
   "queryResultFilter": {
-    "includes": ["field1","field2"...]
+    "includes": ["field1", "field2",..."fieldn"]
   }
-}
+}'
