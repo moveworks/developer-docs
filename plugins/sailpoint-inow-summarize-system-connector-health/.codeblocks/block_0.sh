d5cd1813b555e2589fb6ee8324b03e06fdbf54d7
sjishnu-moveworks <sjishnu@moveworks.com>
2025-11-07 21:52:22 +0530

    Delete plugins/sailpoint-inow-summarize-system-connector-health/.codeblocks/block_0.sh

--- plugins/sailpoint-inow-summarize-system-connector-health/.codeblocks/block_0.sh
+++
@@ -1,3 +0,0 @@
-curl --location 'https://<YOUR_INSTANCE>/v2025/sources?filters=connectorName sw "{{connector}}"' \
---header 'Authorization: Bearer <YOUR_ACCESS_TOKEN>' \
---header 'Accept: application/json'
