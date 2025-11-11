f530b888b7f1a9a432df8b3b6e01a6d15b994121
sbhadwal-moveworks <sbhadwal@moveworks.ai>
2025-11-11 10:49:05 +0530

    Adding code blocks

--- plugins/servicenow-approve-reject-change-request/.codeblocks/block_0.sh
+++ plugins/servicenow-approve-reject-change-request/.codeblocks/block_0.sh
@@ -1,4 +1,4 @@
-curl --location 'https://<API_SERVER_DOMAIN>/api/now/table/sysapproval_approver?sysparm_query=state=requested^approver.email=<USER_EMAIL>^sysapproval.table=change_request&sysparm_fields=sys_id,approver,sysapproval,state' \
+curl --location 'https://<API_SERVER_DOMAIN>/api/now/table/sysapproval_approver' \
 --header 'Authorization: Bearer <Your_Access_Token>' \
 --header 'Accept: application/json' \
 --header 'Content-Type: application/json'
