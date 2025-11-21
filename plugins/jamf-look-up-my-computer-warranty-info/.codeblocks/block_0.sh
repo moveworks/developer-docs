14e1af4d07319e482ec46076ec7dfabb612d0a30
Monivijayababu <monika.a@arbaan.com>
2025-11-21 12:51:48 +0530

    [ARB-325]  Lookup Computer Warranty Info

--- plugins/jamf-look-up-my-computer-warranty-info/.codeblocks/block_0.sh
+++ plugins/jamf-look-up-my-computer-warranty-info/.codeblocks/block_0.sh
@@ -1,4 +1,4 @@
-curl --location 'https://<YOUR_INSTANCE>/api/v1/computers-inventory?section=GENERAL,USER_AND_LOCATION,HARDWARE,GENERAL&filter=userAndLocation.email=="{{email}}"' \
+curl --location 'https://<YOUR_INSTANCE>/api/v1/computers-inventory?section=GENERAL,USER_AND_LOCATION,HARDWARE, DISK_ENCRYPTION,PURCHASING&filter=userAndLocation.email=="{{email}}"' \
 --header 'Content-Type: application/json' \
 --header 'Authorization: Bearer <ACCESS_TOKEN>'
 
