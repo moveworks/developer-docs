e165d77387aa6d7c34325c4a82316618528e146e
Haritha v <haritha.v@arbaan.com>
2025-11-28 18:04:52 +0530

    [ARB-365]_Salesforce_Suggest_Next_Best_Actions_for_an_Opportunity

---
+++ plugins/salesforce-suggest-next-best-actions-for-an-opportunity/.codeblocks/block_0.sh
@@ -0,0 +1,2 @@
+curl --location 'https://{{YOUR_INSTANCE_DOMAIN}}/services/data/v62.0/query?q=SELECT Id, Name, Amount, StageName, Owner.Name, Owner.Email, AccountId, Account.Name, CreatedDate FROM Opportunity WHERE {{filter_query}} ORDER BY LastModifiedDate DESC LIMIT 50' \
+--header 'Authorization: Bearer {{ACCESS_TOKEN}}'
