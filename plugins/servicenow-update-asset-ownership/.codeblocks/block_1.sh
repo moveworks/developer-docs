9374b791a15ca5da41d5e1904c0d56f6afe03641
arunpandian d <arunpandian@arbaan.com>
2025-11-28 17:01:05 +0530

    update: user API changed

--- plugins/servicenow-update-asset-ownership/.codeblocks/block_1.sh
+++ plugins/servicenow-update-asset-ownership/.codeblocks/block_1.sh
@@ -1,3 +1,3 @@
-curl --location 'https://<YOUR_INSTANCE>/api/now/table/sys_user?sysparm_query={{FILTER_QUERY}}&sysparm_fields=sys_id%2Cname%2Cemail%2Cdepartment.name' \
+curl --location 'https://<YOUR_INSTANCE>/api/now/table/sys_user?sysparm_query=email={{EMAIL_ADDRESS}}&sysparm_fields=sys_id%2Cname%2Cemail%2Cdepartment.name' \
 --header 'Authorization: Bearer <ACCESS_TOKEN>' \
 --header 'Accept: application/json'
