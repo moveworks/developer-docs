69e15c71496f9d1c9d5912d24e9b48a42189dcff
madhumitha-arbaan <madhumitha.s@arbaan.com>
2025-12-01 12:20:31 +0530

    [ARB-399] Salesforce - Add Products to an Opportunity

---
+++ plugins/salesforce-add-products-to-an-opportunity/.codeblocks/block_0.sh
@@ -0,0 +1,3 @@
+curl --location 'https://<YOUR_INSTANCE>/services/data/v64.0/query/?q=SELECT+Id%2C+Name%2C+StageName%2C+Amount%2C+CloseDate%2C+LastActivityDate+FROM+Opportunity+WHERE+{{FILTER_QUERY}}+ORDER+BY+LastModifiedDate+DESC+LIMIT+50' \
+--header 'Authorization: Bearer <YOUR_ACCESS_TOKEN>' \
+--header 'Content-Type: application/json' \
