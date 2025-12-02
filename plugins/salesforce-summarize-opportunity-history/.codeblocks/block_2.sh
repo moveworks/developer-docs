54a6b395df6e9d2f64553d8e5b1eb8db89b5b63c
madhumitha-arbaan <madhumitha.s@arbaan.com>
2025-12-02 16:44:14 +0530

    [ARB-386] salesforce-smmarize-opportunity-history

---
+++ plugins/salesforce-summarize-opportunity-history/.codeblocks/block_2.sh
@@ -0,0 +1,28 @@
+{
+  "totalCount": 1,
+  "results": [
+    {
+      "id": "{{opportunity_id}}",
+      "general": {
+        "name": "{{opportunity_name}}",
+        "amount": "{{amount}}",
+        "stageName": "{{stage_name}}",
+        "closeDate": "{{close_date}}",
+        "createdDate": "{{created_date}}",
+        "type": "{{opportunity_type}}",
+        "leadSource": "{{lead_source}}",
+        "probability": "{{probability}}",
+        "forecastCategory": "{{forecast_category}}"
+      },
+      "owner": {
+        "name": "{{owner_name}}",
+        "email": "{{owner_email}}"
+      },
+      "account": {
+        "id": "{{account_id}}",
+        "name": "{{account_name}}"
+      }
+    }
+  ]
+}
+
