7bbae1e6b0a6d2af9b5e2c333241fa90ba5e42de
madhumitha-arbaan <madhumitha.s@arbaan.com>
2025-12-24 12:22:50 +0530

    [ARB-411] Salesforce Sales Rep Performance Analyzer

---
+++ plugins/salesforce-sales-rep-performance-analyzer/.codeblocks/block_2.sh
@@ -0,0 +1,3 @@
+curl --location 'https://<YOUR_INSTANCE>/services/data/v64.0/query/?q=SELECT Id, Name, Email, UserRoleId, UserRole.Name FROM User WHERE Email = '{{email}}'' \
+--header 'Authorization: Bearer <YOUR_ACCESS_TOKEN>' \
+--header 'Content-Type: application/json' \
---
+++ plugins/salesforce-sales-rep-performance-analyzer/.codeblocks/block_4.sh
@@ -0,0 +1,3 @@
+curl --location 'https://<YOUR_INSTANCE>/services/data/v64.0/query/?q=SELECT Id, Name, StageName, CloseDate, Amount, CreatedDate, OwnerId, Owner.Name FROM Opportunity WHERE OwnerId IN ({{owner_id}}) AND CreatedDate >= {{from_date}} AND CreatedDate <= {{to_date}} ORDER BY CreatedDate DESC LIMIT 200' \
+--header 'Authorization: Bearer <YOUR_ACCESS_TOKEN>' \
+--header 'Content-Type: application/json' \
